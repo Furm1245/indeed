108aa4e236245327da78c503d124594c7257bfbc
Pure Crip <walkerfurman@yahoo.com>
2022-03-11 01:09:17 -0500

    med

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import { Fragment } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+import Wrapper from './components/Wrapper/Wrapper';
+import HomePage from './components/StartingPage/StartingPageContent';
+import AuthPage from './pages/AuthPage';
+import DetailPage from './pages/DetailPage';
+import NewJobPage from './pages/NewJobPage';
+import EditPage from './pages/EditPage';
+import Footer from './components/Wrapper/footer';
+import { AuthContext } from './shared/context/auth-context';
+import { useAuth } from './shared/hooks/auth-hook';
+
+
 
 function App() {
+  const { token, login, logout, userId } = useAuth();
+
+
+  let routes;
+  if (token) {
+    routes = (
+      <Wrapper>
+        <Routes>
+          <Route path="/auth" element={<AuthPage />} />
+          <Route path="/new" element={<NewJobPage />} />
+          <Route path="/details/edit/:id" element={<EditPage />} />
+          <Route path="/details/:id" element={<DetailPage />} />
+          <Route path="/:id" element={<HomePage />} />
+          <Route exact path="/" element={<HomePage />} />
+        </Routes>
+        <Footer />
+      </Wrapper>
+
+    );
+  } else {
+    routes = (
+      <Wrapper>
+        <Routes>
+          <Route path="/auth" element={<AuthPage />} />
+          <Route path="/details/edit/:id" element={<EditPage />} />
+          <Route path="/details/:id" element={<DetailPage />} />
+          <Route path="/:id" element={<HomePage />} />
+          <Route exact path="/" element={<HomePage />} />
+        </Routes>
+        <Footer />
+      </Wrapper>
+    );
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: !!token,
+        token: token,
+        userId: userId,
+        login: login,
+        logout: logout
+      }}>
+      <BrowserRouter>
+        <Fragment>
+          {routes}
+        </Fragment>
+      </BrowserRouter>
+    </AuthContext.Provider>
   );
 }
 
